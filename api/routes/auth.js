5473a3a7f79a20ef8ab4645eb9ced7f9c8bca575
alexlecco <alexlecco24@gmail.com>
2020-09-11 11:48:25 -0300

    create login user endpoint

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.post('/register', (req, res) => {
   const { email, password } = req.body
-  
+
   crypto.randomBytes(16, (err, salt) => {
     const newSalt = salt.toString('base64')
     crypto.pbkdf2(password, newSalt, 10000, 64, 'sha1', (err, key) => {
@@ -31,7 +31,23 @@ router.post('/register', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
+  const { email, password } = req.body
 
+  Users.findOne({ email })
+    .exec()
+    .then(user => {
+      if(!user) {
+        return res.send('usuario o contraseña incorrecta')
+      }
+      crypto.pbkdf2(password, user.salt, 10000, 64, 'sha1', (err, key) => {
+        const encryptedPassword = key.toString('base64')
+        if(user.password === encryptedPassword) {
+          const token = signToken(user._id)
+          return res.send({ token })
+        }
+        res.send('usuario o contraseña incorrecta')
+      })
+    })
 })
 
 module.exports = router
